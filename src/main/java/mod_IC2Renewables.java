8c77250624f33e3cd164731a015bc8f551dedd98
Simon Meskens <meskens.simon@gmail.com>
2025-10-06 15:10:13 +0200

    Update version in mod file

--- src/main/java/mod_IC2Renewables.java
+++ src/main/java/mod_IC2Renewables.java
@@ -9,7 +9,7 @@ public class mod_IC2Renewables extends BaseMod {
 
     @Override
     public String Version() {
-        return "1.0.0";
+        return "1.1.0";
     }
 
     /* Mod Menu Information */
